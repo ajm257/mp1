96e8daa2b53d61971ed5deaf2b19b958d13f297b
ajm <ajm257@njit.edu>
2019-02-12 14:29:28 -0500

    Task: Initial Project Setup

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ajm
+ * Date: 2/11/19
+ * Time: 8:22 PM
+ */
+
+echo 'test1234';
---
+++ readme.md
@@ -0,0 +1,3 @@
+#Displayed at the bottom of github
+
+####public is where to put the soucecode of the website
